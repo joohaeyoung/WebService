b60390a7a381ac693962726184e7fc37f58764c4
JinsooSong <https://github.com/JinsooSong/test77.git>
2017-12-07 20:37:31 +0900

    PriceModelMapper 수정

--- src2011136123/src/main/java/koreatech/cse/repository/PriceModelStoreServiceMapper.java
+++ src2011136123/src/main/java/koreatech/cse/repository/PriceModelStoreServiceMapper.java
@@ -1,8 +1,7 @@
 package koreatech.cse.repository;
 
 import koreatech.cse.domain.Food;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +11,15 @@ public interface PriceModelStoreServiceMapper {
 
     @Select("SELECT * FROM PriceModelStoreService")
     List<Food> foodList();
+
+    @Select("SELECT * FROM PriceModelStoreService WHERE SH_NAME={#name}")
+    Food foodByName(@Param("name") String name);
+
+    @Update("UPDATE SET PriceModelStoreService SH_NAME={#shname}, SH_INFO={#shinfo}, SH_PRIDE={#shpride}, SH_ADDR={#shaddr} WHERE ID={#id}")
+    void update(Food food);
+
+    @Delete("DELETE FROM PriceModelStoreService WHERE SH_NAME={#name}")
+    void delete(@Param("name") String name);
 
     @Insert("INSERT INTO PriceModelStoreService (SH_NAME, SH_INFO, SH_PRIDE, SH_ADDR) VALUES (#{shname}, #{shinfo}, #{shpride}, #{shaddr})")
     void insert(Food food);
