b1f7a0925e29ca641b9096d352487cac84765c21
박종희-PC\pjh26 <pjh2688@daum.net>
2017-12-07 22:09:04 +0900

    재수정3

--- src2011136123/src/main/java/koreatech/cse/repository/ConcertPeriodServiceMapper.java
+++ src2011136123/src/main/java/koreatech/cse/repository/ConcertPeriodServiceMapper.java
@@ -40,7 +40,32 @@ import java.util.List;
 @Repository
 public interface ConcertPeriodServiceMapper {
 
-   
+    @Select("SELECT * FROM ConcertPeriodService WHERE TITLE=#{title}")
+    Concert findOneByTitle(@Param("title") String title);
+
+
+    @Select("SELECT * FROM ConcertPeriodService WHERE START_DATE=#{date} OR END_DATE=#{date}")
+    List<Concert> concertStartDateList(@Param("date") String date);
+
+
+    @Select("SELECT * FROM ConcertPeriodService")
+    List<Concert> concertList();
+
+
+    @Insert("INSERT INTO ConcertPeriodService (TITLE, START_DATE, END_DATE, PLACE) VALUES (#{title}, #{startdate}, #{enddate}, #{place})")
+    void insert(Concert concert);
+
+
+    @Update("UPDATE SET ConcertPeriodService TITLE = {#title}, START_DATE = {#startdate}, END_DATE = {#enddate}, PLACE = {#place} WHERE ID = {#id}")
+    void update(Concert concert);
+
+
+    @Delete("DELETE FROM ConcertPeriodService WHERE ID = {#id}")
+    void deleteById(@Param("id") int id);
+
+
+    @Delete("DELETE FROM ConcertPeriodService")
+    void delete();
 
 
 }
